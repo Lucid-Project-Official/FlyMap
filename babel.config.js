95dee5bf9e206ed14de9f028bfabee8da9a9828c
Linoxyr <kestascachard@gmail.com>
2025-11-01 22:07:31 +0100

    Refactor babel.config.js to remove unnecessary plugins and streamline configuration. Update metro.config.js to integrate module aliasing for improved path resolution, enhancing overall project structure and maintainability.

--- babel.config.js
+++ babel.config.js
@@ -10,26 +10,6 @@ module.exports = function (api) {
         },
       ],
     ],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./src'],
-          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-          alias: {
-            '@': './src',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@services': './src/services',
-            '@utils': './src/utils',
-            '@types': './src/types',
-          },
-        },
-      ],
-      // react-native-reanimated doit etre le dernier plugin
-      // Desactive temporairement car newArchEnabled=false
-      // 'react-native-reanimated/plugin',
-    ],
   };
 };
 
--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const { transform } = require('react-native-svg-transformer');
+const path = require('path');
 
 const defaultConfig = getDefaultConfig(__dirname);
 const { assetExts, sourceExts } = defaultConfig.resolver;
@@ -17,6 +18,14 @@ const config = {
   resolver: {
     assetExts: assetExts.filter(ext => ext !== 'svg'),
     sourceExts: [...sourceExts, 'svg'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@screens': path.resolve(__dirname, 'src/screens'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@utils': path.resolve(__dirname, 'src/utils'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    },
   },
 };
 
