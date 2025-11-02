4d3e610f8fe1eb66a660351b5477f1e44cab9101
Linoxyr <kestascachard@gmail.com>
2025-11-02 17:42:04 +0100

    version qui fonctionne !!!

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['@react-native/babel-preset'],
   plugins: [
-    // Par sécurité : strip Flow même si le preset n’est pas appliqué
+    // sécurité : strip Flow
     '@babel/plugin-transform-flow-strip-types',
   ],
 };
--- metro.config.js
+++ metro.config.js
@@ -2,16 +2,12 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
-const config = {
+module.exports = mergeConfig(defaultConfig, {
   transformer: {
-    // ⚠️ Force le transformer officiel qui gère Flow
     babelTransformerPath: require.resolve('@react-native/metro-babel-transformer'),
   },
   resolver: {
-    // Conserve les extensions par défaut
     assetExts: defaultConfig.resolver.assetExts,
     sourceExts: defaultConfig.resolver.sourceExts,
   },
-};
-
-module.exports = mergeConfig(defaultConfig, config);
+});
