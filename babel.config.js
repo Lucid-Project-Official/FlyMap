efcbda07aa4b72a43cc7cf94f23e2dd6db37f258
Linoxyr <kestascachard@gmail.com>
2025-11-02 17:15:04 +0100

    Refactor babel.config.js to simplify configuration by removing the caching function, enhancing clarity and maintainability of the Babel setup for React Native.

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
-module.exports = function (api) {
-  api.cache(false);
-  
-  return {
-    presets: ['module:metro-react-native-babel-preset'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
 };
 
