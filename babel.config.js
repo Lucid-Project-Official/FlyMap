6c0fa53809f2e8011b6b4688971d84ecfe0a6422
Linoxyr <kestascachard@gmail.com>
2025-11-02 17:21:21 +0100

    Remove babel-preset-custom.js and update babel.config.js to enforce Flow type stripping as a default plugin. Upgrade Babel dependencies in package.json and package-lock.json for improved compatibility and performance.

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    // Force le strip Flow même si le preset ne s'applique pas (sécurité)
+    '@babel/plugin-transform-flow-strip-types',
+  ],
 };
 
